39f156794a8c148c1e4bca295cd5393e6fa38750
woominShim <worejj95@gmail.com>
2024-07-11 20:47:25 +0900

    organize dir

--- src/sort/BOJ10825.java
+++ src/sort/BOJ10825.java
@@ -1,9 +1,8 @@
-package baekjoon;
+package sort;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
--- src/sort/BOJ11652.java
+++ src/sort/BOJ11652.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package sort;
 
 import java.io.BufferedReader;
 import java.io.IOException;

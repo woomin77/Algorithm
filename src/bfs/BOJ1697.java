612361c9d45daefffd1679c366694be547afa494
woominShim <worejj95@gmail.com>
2024-07-16 23:53:57 +0900

    BOJ1697 구현중

---
+++ src/bfs/BOJ1697.java
@@ -0,0 +1,74 @@
+package bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ1697 {
+
+    static int N, K;
+    static int[] dist;  //이동 횟수 체크
+    static boolean[] visit;
+
+    static int atoi(String st) {
+        return Integer.parseInt(st);
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = atoi(st.nextToken());
+        K = atoi(st.nextToken());
+        visit = new boolean[100001];
+        dist = new int[100001];
+    }
+
+    //5 10 9 18 17
+    static void bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+
+        que.add(start);
+        dist[start] = 0;  //시작 위치의 이동 횟수는 0
+        visit[start] = true;
+
+        while (!que.isEmpty()) {
+            int x = que.poll();
+
+            //+1, -1, *2 상태 그래프 만들기
+            int x1 = x + 1;
+            if (!(x1 > 100000)) {
+                que.add(x1);
+                dist[x1] = dist[x] + 1;
+            }
+
+            int x2 = x - 1;
+            if(!(x2 < 0)) {
+                que.add(x2);
+                dist[x2] = dist[x] + 1;
+            }
+
+            int x3 = x * 2;
+            if (!(x3 > 100000)) {
+                que.add(x3);
+                dist[x3] = dist[x] * 2;
+            }
+        }
+
+
+
+    }
+
+    static void solution() {
+        bfs(N);   // 수빈이가 있는 위치 부터 시작
+        System.out.println(dist[K]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+}
